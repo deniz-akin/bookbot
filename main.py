255ab5aa1dffedfb2ee1be28e96e54530620e00c
Belthronding01 <denizakin113@gmail.com>
2025-01-11 15:39:44 +0300

    added .gitignore file and wrote a program that counts the characters

---
+++ main.py
@@ -0,0 +1,42 @@
+def sort_on(dict):
+    return dict["num"]
+
+def convert_to_list(dicts):
+    dicts_list = []
+    for char in dicts:
+        new_dict = {'char': char, 'num': dicts[char]}
+        dicts_list.append(new_dict)
+    return dicts_list
+
+def print_report(dict):
+    dict = convert_to_list(dict)
+    dict.sort(reverse = True, key=sort_on)
+    for chars in dict:
+        if chars['char'].isalpha():
+            print(f"The '{chars['char']}' character was found {chars['num']} times")
+    print("---End report---")
+    
+def count_chars(text):
+    lowercased = text.lower()
+    chars = {}
+    for char in lowercased:
+        if char not in chars:
+            chars[char] = 1
+        else:
+            chars[char] += 1
+    return chars
+def word_count(text):
+    words = text.split()
+    return(len(words))
+
+def main():
+    with open('books/frankenstein.txt', 'r', encoding="utf-8") as f:
+        file_contents = f.read()
+    print("--- Begin report of books/frankenstein.txt ---")
+    lowercased = file_contents.lower()
+    count = word_count(lowercased)
+    print(f"{count} words found in the document\n")
+    chars = count_chars(lowercased)
+    print_report(chars)
+
+main()
